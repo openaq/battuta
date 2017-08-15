edc0adfbad9278acc1b4b9d4e4b027b1feea34d4
maxgrossman <m.grossman1813@gmail.com>
2017-08-15 14:36:34 -0400

    move build to master branch / remove history

--- make-station-history.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "Removing yestderay's stations from history and adding today's..."
-DATE=`date +%Y-%m-%d`
-rm -f ./history/*
-cp ./eea-stations.json ./history/eea-stations-${DATE}.json
-echo "...We've made history!"
